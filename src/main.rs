c08a55ba27c2a9c51c65ddece424e6f0f3706ef8
Dustin <dustin.tran@live.com>
2018-08-30 14:56:12 +0900

    created basic working struct for node

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 //graph class
+struct node{
+    name: char,
+    connections: Vec<char>
+}
 
+impl node {
+    fn add_connection(&mut self, conn: char) {
+        self.connections.push(conn);
+    }
+}
 
 
 fn main() {
-    //call graph class?
+    let mut nodeA = node {
+        name: 'a',
+        connections: Vec::new()
+    };
+
+    nodeA.add_connection('b');
+
+    println!("node A {}", nodeA.name);
+    for i in &nodeA.connections {
+        println!("connections: {}", i);
+    }
+    println!("hello, world!");
 }
